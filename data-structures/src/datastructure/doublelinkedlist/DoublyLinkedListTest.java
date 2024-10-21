d8cf7a78731bbe4f00064e4ec10d35308974c66c
Abhijeet S <selvester69@gmail.com>
2024-10-22 00:05:07 +0530

    updated practice code for LL and DLL

--- data-structures/src/datastructure/doublelinkedlist/DoublyLinkedListTest.java
+++ data-structures/src/datastructure/doublelinkedlist/DoublyLinkedListTest.java
@@ -1,5 +1,21 @@
 package datastructure.doublelinkedlist;
 
+import datastructure.doublelinkedlist.DoublyLinkedList.Node;
+
 public class DoublyLinkedListTest {
 
+    public static void main(String[] args) {
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+        myDLL.append(1);
+        myDLL.append(2);
+        myDLL.append(3);
+        myDLL.append(4);
+        myDLL.append(6);
+        myDLL.append(7);
+        myDLL.printAll();
+
+        // Node removed = myDLL.get(4);
+        System.out.println("get = "+myDLL.get(myDLL.length/2).value);
+        myDLL.printList();
+    }
 }
