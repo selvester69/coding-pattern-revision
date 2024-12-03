5f67af630978d881bb8b781215ebfe29bc866b9d
Abhijeet S <selvester69@gmail.com>
2024-12-04 00:35:32 +0530

    square of sorted Array

---
+++ Algorithms/src/leetcode/SquaresOfSortedArray.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.Arrays;
+// Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+public class SquaresOfSortedArray {
+
+    public static void main(String[] args) {
+        int[]res = SquaresOfSortedArray.sortedSquares(new int[]{-4,-1,0,3,10});
+        // int[]res = SquaresOfSortedArray.sortedSquares(new int[]{-5,-3,-2,-1});
+        Arrays.stream(res).forEach(item-> System.out.print(item+" "));
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        int[] res = new int[nums.length];
+        int start = 0,end = nums.length-1;
+        int count = nums.length-1;
+        while(start<=end){
+            if(nums[start]*nums[start] >= nums[end]*nums[end] ){
+                res[count--] = nums[start]*nums[start];
+                start++;
+            }else {
+                res[count--] = nums[end]*nums[end];
+               end--;
+            }
+        }
+        return res;
+    }
+
+}
