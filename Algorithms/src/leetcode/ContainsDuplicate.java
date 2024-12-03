939756b80f5a373b36b59837be32b6c16cd64daf
Abhijeet S <selvester69@gmail.com>
2024-12-03 23:06:53 +0530

    problems updated

---
+++ Algorithms/src/leetcode/ContainsDuplicate.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        boolean res = ContainsDuplicate.containsDuplicate(new int[] {1,1,1,3,3,4,3,4,2});
+        System.out.println(res); 
+    }
+
+
+    public static boolean containsDuplicate(int[] nums) {
+         Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+
+    public static boolean containsDuplicateBruteForce(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ Algorithms/src/leetcode/MoveZeros.java
@@ -0,0 +1,125 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class MoveZeros {
+
+    public static void main(String[] args) {
+        int nums[] = new int[] { 1, 2, 4, 0, 1, 0, 3, 12 };
+        // int nums[] = new int[] { 2,1 };
+        MoveZeros.moveZeroes(nums);
+        Arrays.stream(nums).forEach(item -> System.out.print(item + " "));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int count = 0;
+        for (int num: nums) {
+            if (num != 0) nums[count ++] = num;
+        }
+        for (int i = count; i < nums.length; i ++) nums[i] = 0;
+    }
+    
+    public static void moveZeroesApproach3(int[] nums) {
+        int i = 0, j = 0;
+        while (j != nums.length) {
+            if (nums[j] != 0) {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+                i++;
+            }
+            j++;
+        }
+    }
+
+    public static void moveZeroes2msSolution(int[] nums) {
+        int n = nums.length;
+        if (n < 2)
+            return;
+        int i = 0, j = i + 1;
+        while (i < n && nums[i] != 0) {
+            i++;
+        }
+        while (i < n) {
+            if (j < i) {
+                j = i;
+            }
+            while (j < n && nums[j] == 0) {
+                j++;
+            }
+            if (j < n && nums[j] != 0) {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+            }
+            i++;
+        }
+
+    }
+
+    public static void moveZeroes2msSolutionApproach2(int[] nums) {
+        int n = nums.length;
+        if (n < 2)
+            return;
+        int i = 0, j = -1;
+        while (i < n) {
+            if (nums[i] != 0) {
+                i++;
+                continue;
+            }
+            if (j < i) {
+                j = i;
+            }
+            while (j < n && nums[j] == 0) {
+                j++;
+            }
+            if (j < n && nums[j] != 0) {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+            }
+            i++;
+        }
+    }
+
+    public static void moveZeroesApproach2(int[] nums) {
+        if (nums.length < 2)
+            return;
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            while (i < nums.length && nums[i] != 0) {
+                i++;
+            }
+            j = i + 1;
+            while (j < nums.length && nums[j] == 0) {
+                j++;
+            }
+            if (j < nums.length && i < nums.length) {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+                i++;
+            }
+        }
+    }
+
+    public static void moveZeroesBruteForce(int[] nums) {
+        if (nums.length < 2)
+            return;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != 0) {
+                continue;
+            }
+            inner: for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] != 0) {
+                    int t = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = t;
+                    break inner;
+                }
+            }
+        }
+    }
+
+}
