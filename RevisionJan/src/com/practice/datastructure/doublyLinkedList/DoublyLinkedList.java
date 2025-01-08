dff86e0b63284ab3d3f577a4475638563f758d11
abhijeet <selvester69@gmail.com>
2025-01-08 22:24:30 +0530

    jan Revision

--- RevisionJan/src/com/practice/datastructure/doublyLinkedList/DoublyLinkedList.java
+++ RevisionJan/src/com/practice/datastructure/doublyLinkedList/DoublyLinkedList.java
@@ -79,4 +79,104 @@ public class DoublyLinkedList {
         return temp;
     }
 
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+        this.length++;
+    }
+
+    public Node removeFirst() {
+        if (this.head == null)
+            return this.head;
+        Node temp = this.head;
+        if (this.length == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+            temp.next = null;
+            this.head.prev = null;
+        }
+        this.length--;
+        return temp;
+    }
+
+    // in case of doubly linked list iterate from end if index>length/2 to reduce
+    // the time complexity
+    public Node get(int index) {
+        if (index < 0 || index >= this.length)
+            return null;
+        Node temp = null;
+        if (index < length / 2) {
+            temp = this.head;
+            while (index > 0) {
+                temp = temp.next;
+                index--;
+            }
+        } else {
+            temp = this.tail;
+            while (length - index - 1 > 0) {
+                temp = temp.prev;
+                index++;
+            }
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > this.length)
+            return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        } else if (index == this.length) {
+            append(value);
+            return true;
+        } else {
+            Node newNode = new Node(value);
+            Node prev = get(index - 1);
+            newNode.next = prev.next;
+            prev.next = newNode;
+            newNode.next.prev = newNode;
+            newNode.prev = prev;
+            this.length++;
+            return true;
+        }
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= this.length)
+            return null;
+        if (index == 0) {
+            return removeFirst();
+        } else if (index == length - 1) {
+            return removeLast();
+        } else {
+            Node prev = get(index - 1);
+            Node temp = prev.next;
+            prev.next = temp.next;
+            prev.next.prev = prev;
+            temp.prev = null;
+            temp.next = null;
+            this.length--;
+            return temp;
+        }
+    }
+
 }
