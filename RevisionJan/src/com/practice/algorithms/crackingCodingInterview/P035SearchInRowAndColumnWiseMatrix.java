dab23d434023d2b2a7c2ff64b8eea7732b41f552
abhijeet <selvester69@gmail.com>
2025-01-27 22:42:11 +0530

    revision Jan

---
+++ RevisionJan/src/com/practice/algorithms/crackingCodingInterview/P035SearchInRowAndColumnWiseMatrix.java
@@ -0,0 +1,24 @@
+package com.practice.algorithms.crackingCodingInterview;
+
+public class P035SearchInRowAndColumnWiseMatrix {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i >= 0 && i < m && j >= 0 && j < n) {
+            if (target == matrix[i][j]) {
+                return true;
+            } else if (target > matrix[i][j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}}, 3));
+        System.out.println(searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}}, 13));
+    }
+}
